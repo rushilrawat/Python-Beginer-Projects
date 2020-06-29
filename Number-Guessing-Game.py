fd83bb177fe29a3946ece76137d8639fccd14c01
Rushil Rawat <61906345+RushilRawat@users.noreply.github.com>
2020-06-29 17:19:38 +0530

    Update Number-Guessing-Game.py

--- Number-Guessing-Game.py
+++ Number-Guessing-Game.py
@@ -1,11 +1,11 @@
 #Number Guessing Game
 
 Secret_Number = int(input("Please give a secret number:"))
-Max_Number_Limit = int(input("Please give the maximun number limit"))
-Lowest_Number_Limit = int(input("Please give the lowest number limit"))
-Guess_Limit = int(input("Please give guess limit:"))
+Guess_Limit = int(input("Please give guess limit (ie. How many chances) :"))
 Guess_Count = 0
-print(F"Hint : The Secert Number is between {Lowest_Number_Limit} to {Max_Number_Limit}")
+
+print(F"Hint : The Secert Number is between {Secret_Number - 50} to {Secret_Number + 50}")
+
 while Guess_Count != Guess_Limit:
     Guess=int(input("Guess a number:"))
     Guess_Count += 1
