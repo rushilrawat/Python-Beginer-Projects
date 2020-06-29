35a0579d95bd13beac87ea504c66ccab133d17f9
Rushil Rawat <61906345+RushilRawat@users.noreply.github.com>
2020-06-29 19:19:35 +0530

    Update Number-Guessing-Game.py

--- Number-Guessing-Game.py
+++ Number-Guessing-Game.py
@@ -1,18 +1,20 @@
 #Number Guessing Game
+
 from getpass import getpass
+
 Secret_Number = int(getpass("Please give a secret number:"))
 Guess_Limit = int(input("Please give guess limit (ie. How many chances) :"))
-Guess_Count = 0
 
 print(F"Hint : The Secert Number is any number between {Secret_Number - 60} to {Secret_Number + 60}")
+print(f"You have {Guess_Limit} Chances to Guess the Secret Number" )
 
-while Guess_Count != Guess_Limit:
+while Guess_Limit != 0:
     Guess=int(input("Guess a number:"))
-    Guess_Count += 1
+    Guess_Limit -= 1
     if Guess != Secret_Number:
-        print("Try Again!")
+        print(f"Wrong, Try Again! and be careful, You only have {Guess_Limit} Chances Left")
     elif Guess== Secret_Number:
-        print(f"Your Guess was right, The secret Number was {Secret_Number}. You wont the game!")
+        print(f"Your Guess was right, The secret Number was {Secret_Number}. You won the game!")
         break
 else:
     print(f"Sorry! None of your guesses were right, The secret Number was {Secret_Number}. You failed!")
